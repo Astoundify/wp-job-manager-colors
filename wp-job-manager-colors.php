19f36953bc00e8ff76590944cd9bbc6bc9880afa
Spencer Finnell <spencerfinnell@gmail.com>
2013-07-18 12:41:28 -0400

    Color pickers. Closes #1

--- wp-job-manager-colors.php
+++ wp-job-manager-colors.php
@@ -30,6 +30,11 @@ final class WP_Job_Manager_Colors {
 	private function setup_actions() {
 		add_filter( 'job_manager_settings', array( $this, 'job_manager_settings' ) );
 		add_action( 'wp_head', array( $this, 'output_colors' ) );
+
+		if ( is_admin() ) {
+			add_action( 'admin_enqueue_scripts', array( $this, 'colorpickers' ) );
+			add_action( 'admin_footer', array( $this, 'colorpickersjs' ) );
+		}
 	}
 
 	public function job_manager_settings( $settings ) {
@@ -64,14 +69,18 @@ final class WP_Job_Manager_Colors {
 				'std' 		  => '',
 				'placeholder' => '#',
 				'label' 	  => '<strong>' . $term->name . '</strong>',
-				'desc'		  => __( 'Hex value for the color of this job type.', 'job_manager_colors' )
+				'desc'		  => __( 'Hex value for the color of this job type.', 'job_manager_colors' ),
+				'attributes'  => array(
+					'data-default-color' => '#fff',
+					'data-type'          => 'colorpicker'
+				)
 			);
 		}
 
 		return $options;
 	}
 
-	function output_colors() {
+	public function output_colors() {
 		$terms   = get_terms( 'job_listing_type', array( 'hide_empty' => false ) );
 
 		echo "<style id='job_manager_colors'>\n";
@@ -83,6 +92,30 @@ final class WP_Job_Manager_Colors {
 		}
 
 		echo "</style>\n";
+	}
+
+	public function colorpickers( $hook ) {
+		$screen = get_current_screen();
+		
+		if ( 'job_listing_page_job-manager-settings' != $screen->id )
+			return;
+
+		wp_enqueue_script( 'wp-color-picker' );
+		wp_enqueue_style( 'wp-color-picker' );
+	}
+
+	public function colorpickersjs() {
+		$screen = get_current_screen();
+		
+		if ( 'job_listing_page_job-manager-settings' != $screen->id )
+			return;
+		?>
+			<script>
+				jQuery(document).ready(function($){
+					$( 'input[data-type="colorpicker"]' ).wpColorPicker();
+				});
+			</script>
+		<?php
 	}
 }
 
